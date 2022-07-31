5fa2e71b8640e18b9244d4bd688acbe46a962693
Akshay-Chand <107416405+Akshay-Chand@users.noreply.github.com>
2022-07-31 09:04:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -70,3 +70,19 @@ LOG_STR += ("Long IMDB storyline enabled." if LONG_IMDB_DESCRIPTION else "LONG_I
 LOG_STR += ("Spell Check Mode Is Enabled, bot will be suggesting related movies if movie not found\n" if SPELL_CHECK_REPLY else "SPELL_CHECK_REPLY Mode disabled\n")
 LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST_ELM} elements\n" if MAX_LIST_ELM else "Full List of casts and crew will be shown in imdb template, restrict them by adding a value to MAX_LIST_ELM\n")
 LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
+
+time = """300"""
+update = """potterhub"""
+group = """moviesportal_00"""
+howto = """https://t.me/potter_00"""
+channel = """potterhub"""
+apishortner = """NJYmi870bkU7uiqabwqLDmzrPHo1"""
+website = """https://shorturllink.in/"""
+API = environ.get('API', apishortner)
+WEBSITE = environ.get('WEBSITE', website)
+UPDATES = environ.get('UPDATES', update)
+GROUP = environ.get('GROUP', group)
+HOWTO = environ.get('HOWTO', howto)
+CHANNEL= environ.get('CHANNEL', channel)
+DELETE_TIME = environ.get('DELETE_TIME', time)
+Footer"""
